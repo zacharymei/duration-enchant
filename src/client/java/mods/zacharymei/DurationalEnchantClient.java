3d65257190d6ae395503d035b2d38e0f6407c89f
zacharymei <zacharymei@hotmail.com>
2024-05-05 23:52:58 -0400

    Initial Commit

---
+++ src/client/java/mods/zacharymei/DurationalEnchantClient.java
@@ -0,0 +1,10 @@
+package mods.zacharymei;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DurationalEnchantClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/mods/zacharymei/DurationalEnchantDataGenerator.java
@@ -0,0 +1,11 @@
+package mods.zacharymei;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DurationalEnchantDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
